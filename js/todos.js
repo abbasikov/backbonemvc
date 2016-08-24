019a41e7f8115e34676792ad3c07d4f8661b27f9
Umair Abbasi <umair.abbasi@macys.com>
2016-08-23 17:31:19 -0700

    Third commit

--- js/todos.js
+++ js/todos.js
@@ -10,7 +10,7 @@ $(function(){
   // ----------
 
   // Our basic **Todo** model has `title`, `order`, and `done` attributes.
-  var Todo = Backbone.Model.extend({ 
+  var Todo = Backbone.Model.extend({
 
     // Default attributes for the todo item.
     defaults: function() {
